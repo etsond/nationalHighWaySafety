9b05b01bbd427a35c8a778e31fa412eba9497402
Etson <etsond@ufl.edu>
2023-02-27 11:19:55 -0500

    adding an event listener to get the vehicle from user

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 
+document.querySelector("button").addEventListener("click", getVehicle)
 
-fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/440?format=json')
+function getVehicle(){
+
+let makeId = Number(document.querySelector("input").value)
+
+fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/${makeId}?format=json`)
     .then(res => res.json())
     .then(data => {
         console.log(data)
@@ -8,3 +13,5 @@ fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/440?format=jso
     .catch(err => {
         console.log(`error ${err}`)
     })
+
+}
