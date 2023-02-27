cb797911584a9dc9b92a1a42ecb61484402d1408
Etson <etsond@ufl.edu>
2023-02-27 11:29:15 -0500

    getting the name

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/${makeId}?form
     .then(res => res.json())
     .then(data => {
         console.log(data)
+        document.querySelector("h1").innerText = data.Message
+        console.log(data.Message)
+        console.log(data.Results)
+        document.querySelector("p").innerText = data.Results[0].Make_Name
     })
     .catch(err => {
         console.log(`error ${err}`)
