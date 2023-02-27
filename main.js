94ff42b1cc109ed029a28e7d0444f4fa30da8d7c
Etson <etsond@ufl.edu>
2023-02-27 11:12:36 -0500

    testing the api is successfull

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 
 
 fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/440?format=json')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
