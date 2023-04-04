91911436d9cc727ff039098f8adbdd55ab79ada0
Etson <etsond@ufl.edu>
2023-04-04 16:13:13 -0400

    adding some stylings

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
 
-document.querySelector("button").addEventListener("click", getVehicle)
+document.querySelector("button").addEventListener("click", getVehicle);
+document.querySelector("input").addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+    getVehicle();
+  }
+});
 
-function getVehicle(){
+function getVehicle() {
+  let makeId = Number(document.querySelector("input").value);
 
-let makeId = Number(document.querySelector("input").value)
-
-fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/${makeId}?format=json`)
-    .then(res => res.json())
+  fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/${makeId}?format=json`)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      return res.json();
+    })
     .then(data => {
-        console.log(data)
-        // document.querySelector("h1").innerText = data.Message
-        console.log(data.Message)
-        console.log(data.Results)
-        // document.querySelector("p").innerText = data.Results[0].Make_Name
+      console.log(data);
+      if (data.Results.length === 0) {
+        document.querySelector("p").innerText = "We do not have this current make Id. Please try another one.";
+
+      } else {
+        document.querySelector("p").innerText = "";
         data.Results.forEach(element => {
-            console.log(element.Make_ID)
-            document.querySelector("#id").innerText = `Make Id is ${element.Make_ID}`
-            console.log(element.Make_Name)
-            document.querySelector("h4").innerText = `Make Name is ${element.Make_Name}`
-            console.log(element.Model_Name)
-            document.querySelector("#modelName").innerText = `Model Name is ${element.Model_Name}` 
-            
+          console.log(element.Make_ID);
+          document.querySelector("#id").innerText = `Make Id is ${element.Make_ID}`;
+          console.log(element.Make_Name);
+          document.querySelector("h4").innerText = `Make Name is ${element.Make_Name}`;
+          console.log(element.Model_Name);
+          document.querySelector("#modelName").innerText = `Model Name is ${element.Model_Name}`;
         });
+      }
     })
     .catch(err => {
-        console.log(`error ${err}`)
-    })
-
+      console.log(`error ${err}`);
+      document.querySelector("p").innerText = `Error: ${err.message}. Please try again later.`;
+    });
 }
